61fb1212399ec05062401ab782392ed5b627f4db
maghandas <dasmaghan786@gmail.com>
2024-02-26 00:04:53 +0500

    variables in js

--- variables.js
+++ variables.js
@@ -4,7 +4,7 @@ let num = 18;
 // sometime var is also used , var was used in older times.
 // in modern javascript, we prefer let
 console.log(num);
-
+// ************************************************ *******
 // using const-- const is used with values which can't be modified further 
 const pi = 3.14;
 // pi = 33; // not allowed.
