1ec88a85eaa1411fe5f98951b7ebdbb3c9905ca7
maghandas <dasmaghan786@gmail.com>
2024-02-25 22:06:43 +0500

    learnt about let

--- variables.js
+++ variables.js
@@ -1,5 +1,7 @@
 
 // variables are used to store values...
-let name = "maghan"
+let name = "maghan";
+// sometime var is also used , var was used in older times.
+// in modern javascript, we prefer let
 
 console.log(name);
