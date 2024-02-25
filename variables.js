d4c0c500bdfb4d35b854c4721a0021b8809ec84f
maghandas <dasmaghan786@gmail.com>
2024-02-25 22:11:23 +0500

    learnt about let,var,constt

--- variables.js
+++ variables.js
@@ -1,7 +1,11 @@
 
 // variables are used to store values...
-let name = "maghan";
+let num = 18;
 // sometime var is also used , var was used in older times.
 // in modern javascript, we prefer let
+console.log(num);
 
-console.log(name);
+// using const-- const is used with values which can't be modified further 
+const pi = 3.14;
+// pi = 33; // will give error 
+console.log(pi);
