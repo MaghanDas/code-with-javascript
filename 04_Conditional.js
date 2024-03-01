f5ad078e5b7859a6dcf2d0238bce47e2be7df2dd
maghandas <dasmaghan786@gmail.com>
2024-03-02 00:05:11 +0500

    updated conditionals

--- 04_Conditional.js
+++ 04_Conditional.js
@@ -1,7 +1,7 @@
 
-// if(12 > 3) {
-//     console.log("12 is greater than 3");
-// }
-// else
-// console.log("12 is less than 3");
+if(12 > 3) {
+    console.log("12 is greater than 3");
+}
+else
+console.log("12 is less than 3");
 
