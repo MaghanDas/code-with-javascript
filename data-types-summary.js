4ef61c444561fd17de2f2325a6a51658e406c50e
maghandas <dasmaghan786@gmail.com>
2024-03-04 21:50:22 +0500

    updated datatypes

---
+++ data-types-summary.js
@@ -0,0 +1,19 @@
+
+// primitve datatypes:
+// String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+const score = 100; // Number
+const scoreValue = 100.2 // Number
+// const bigNumber = 22323252n;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let useremail; 
+
+const id = Symbol('123'); 
+// non-primitive datatypes or reference type::
+// Arrays, Objects, Functions...
+
+// js is static typed language.
+
+// 
