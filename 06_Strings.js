f1fd216dbffd79a01932a40567776693c55e50d7
maghandas <dasmaghan786@gmail.com>
2024-03-07 22:21:34 +0500

    updated strings.js

--- 06_Strings.js
+++ 06_Strings.js
@@ -2,4 +2,13 @@
 let name = "Maghan";
 let age = 21;
 
+// console.log("maghan" + age);
 console.log(`My name is ${name} and i am ${21} years old`);
+
+const nam = new String("Maghan");
+console.log(nam.length);
+console.log(nam.toUpperCase);
+console.log(nam.charAt(3));
+// other method
+// substring, slice
+
