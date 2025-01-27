e8a0a369444b611c0039d2fa54b654cdb6ff0bbb
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 02:54:13 +0000

    maths in js

---
+++ 07_maths.js
@@ -0,0 +1,26 @@
+
+const score = 500;
+
+// console.log(score);
+// console.log(score.toString().length);
+// console.log(score.toFixed(1)); // 500.0
+
+const otherNum = 12.21212;
+// console.log(otherNum.toPrecision(3)); // 12.2
+
+// careful to use toPrecision.. value should be greater than digits before decimal in number.
+
+const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IN')); // 1,00,000
+
+// NUmber. (some method with NUmber dataType..)
+
+console.log(Math); // OBject
+console.log(Math.abs(-2)); // 2
+console.log(Math.round(4.6)); // 5
+console.log(Math.ceil(4.2)); // takes upper value . // 5
+console.log(Math.floor(4.9)); // takes lover value // 4
+
+
+
+
