08e1fdeab59a7681e41cc42671af101a62981fa1
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 03:20:30 +0000

    data types

---
+++ Basics/02_data_types.js
@@ -0,0 +1,23 @@
+
+// type of values that a variable can hold is know as datatypes...
+// String   -- let name  = "Maghan";    
+//  number => 2 to power to 53
+// bigint
+// boolean => true or false .
+// null => standalone value
+// undefined => it means value is not given yet..!!
+// symbol =>
+// ****************************************************************
+// type of undefined is undefined.
+// type of null is Object
+// ****************************************************************
+let name = "Maghan";
+let number = 18;
+let hasGithubAcc = true;
+let username = null;
+let email;
+
+console.table([typeof name, typeof number, typeof hasGithubAcc,  typeof username, typeof email]);
+
+console.log(typeof undefined) // undefined..
+
