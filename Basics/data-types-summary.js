40e04e0d716a1a43b1a15775ccfb29ae892a4fe4
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 03:22:01 +0000

    dataTypes summary

---
+++ Basics/data-types-summary.js
@@ -0,0 +1,37 @@
+
+// primitve datatypes:
+// String, Number, Boolean, null, undefined, Symbol, BigInt.
+
+const score = 100; // Number
+const scoreValue = 100.2 // Number
+// const bigNumber = 22323252n;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let useremail; 
+
+const id = Symbol('123'); 
+// non-primitive datatypes or reference type::
+// Arrays, Objects, Functions...
+
+ const heros = ["ironMan" , "SpiderMan" , "Thor"];
+
+ let myObj = {
+    name:"Maghan",
+    age:20,
+ }
+
+ const muFunction = function() {
+      console.log("hyee :)");
+ }
+
+// js is static typed language.
+console.log(typeof myObj);  // object
+console.log(typeof heros);  // object
+console.log(typeof muFunction); // function..
+
+
+// *****************************************************
+// memory- stack vs heap
+// permtive types uses stack memory,  we get copy of the data.
+// non-primitve uses heap memory/ // we get reference of the original data.
