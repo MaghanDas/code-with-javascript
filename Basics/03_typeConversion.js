1053156cef4a366a390b3bbcbae5d30a3a7e311c
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 03:20:51 +0000

    type conversions

---
+++ Basics/03_typeConversion.js
@@ -0,0 +1,35 @@
+// let value = undefined;
+// let value = null;
+
+let value = "22";
+
+// console.log(typeof value);
+// let valueInNumber = Number(value);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "53" to 53
+// "53abc" to NaN (type is number)
+// true to 1; false => 0
+
+// for conversion of Boolean.
+// 1 to true, 0 to false
+// {} to false, "string" to true.
+
+// console.log("2" + 1); // 21
+// console.log(1 + "2");  // 12
+// console.log("1" + 2 + 2 ); //122
+// console.log(1 + 2 + "2"); // 32
+
+
+// console.log(true);  // true
+// console.log(+true); // 1
+
+// console.log(null > 0);  // false
+// console.log(null == 0); // false 
+// console.log(null >= 0); // true-> 
+
+// === strict check -> checks dataypes & value itself!
+// console.log("2" === 2);
+
+
