dcfdd6f9df9b32ca6fe29af95c76aebed1448c1d
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 03:43:33 +0000

    arrays in js

---
+++ Arrays/arrays.js
@@ -0,0 +1,17 @@
+
+//  deep copy -> do not share the same reference.. 
+//  shallow copy -> share the same reference.. 
+
+const nums = [0, 1, 2, 3, 4, 5];
+const heros = ["shaktiman", "nagraj"];
+
+let a =  [];
+
+// console.log(a);
+// console.log(nums)
+// console.log(heros);
+
+let array = new Array(10,20,30);
+console.log(array);
+
+
