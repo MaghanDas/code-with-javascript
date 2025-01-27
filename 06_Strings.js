9c01bdd0d36d07e6765d9cfadb53e880a3100318
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 02:28:56 +0000

    strings js

--- 06_Strings.js
+++ 06_Strings.js
@@ -17,3 +17,10 @@ const anotherString = nam.slice(-8,4)
 console.log(anotherString)
 
 const newStrin1 = " das "
+console.log(newStrin1.trim());
+
+const url = "http:/wwww.com"
+
+console.log(url.replace('%20','-' ))
+
+ 
