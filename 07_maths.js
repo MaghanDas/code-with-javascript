02c93ac423f76ce99cbb2dbc582b012d9d6c0327
Maghan Das <dasmaghan786@gmail.com>
2025-01-27 03:01:50 +0000

    maths in js

--- 07_maths.js
+++ 07_maths.js
@@ -15,11 +15,27 @@ const hundreds = 100000;
 
 // NUmber. (some method with NUmber dataType..)
 
-console.log(Math); // OBject
-console.log(Math.abs(-2)); // 2
-console.log(Math.round(4.6)); // 5
-console.log(Math.ceil(4.2)); // takes upper value . // 5
-console.log(Math.floor(4.9)); // takes lover value // 4
+// console.log(Math); // OBject
+// console.log(Math.abs(-2)); // 2
+// console.log(Math.round(4.6)); // 5
+// console.log(Math.ceil(4.2)); // takes upper value . // 5
+// console.log(Math.floor(4.9)); // takes lover value // 4
+// console.log(Math.max(4,3,4,5,6)); 
+// console.log(Math.min(4,3,4,5,6)); 
+
+console.log(Math.random());
+console.log((Math.random()*10 )+ 1);
+console.log(Math.floor((Math.random()*10 )+ 1));
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max-min + 1))+ min);
+
+
+
+
 
 
 
