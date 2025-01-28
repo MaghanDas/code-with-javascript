52ad162f338a9764c45b2e0d5b2b2ac452c54ddc
Maghan Das <dasmaghan786@gmail.com>
2025-01-28 23:12:48 +0000

    functions in js

---
+++ Functions/01_functions.js
@@ -0,0 +1,20 @@
+
+
+function functionName(paramete1,parameter2) {
+  // statements..    
+}
+
+function sum(num1, num2){
+    console.log(num1+num2);
+}
+
+function sum2(num1,num2) {
+    return num1+num2;
+}
+// sum(null,null); // 0
+// sum(2,3);
+
+// const result = sum(3,5);
+// console.log(result); // undefined...
+
+console.log(sum2(2,3)); // 5
