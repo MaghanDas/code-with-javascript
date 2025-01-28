2aacaa79ca3a53226a0c336009d73e11a0efc9c3
Maghan Das <dasmaghan786@gmail.com>
2025-01-28 21:19:19 +0000

    more on arrays in js

--- Arrays/02_arrays.js
+++ Arrays/02_arrays.js
@@ -4,5 +4,17 @@ const movies = ["PK", "IronMan", "fastX"];
 
 const Netflix = webSeries.concat(movies);
 // concat combines and returns new arr..
-console.log(Netflix);
+// console.log(Netflix);
+
+const netFlix2 = [...webSeries, ...movies];
+// console.log(netFlix2); // combines both with spread operator(...)
+
+const dumArr = [1,3,4,[0,1,2],4,[3,3,2],9];
+// console.log(dumArr);
+const allDumArr = dumArr.flat(Infinity); // makes it one array , no subArr
+// console.log(allDumArr); // 
+
+console.log(Array.isArray("string")); // false
+console.log(Array.from("string")); // makes it array..
+console.log(Array.from({name: "name"})) // important ...
 
