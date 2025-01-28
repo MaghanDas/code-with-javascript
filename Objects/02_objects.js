64f85732db9b21227590e29343ab4ddbfa3e4a9f
Maghan Das <dasmaghan786@gmail.com>
2025-01-28 21:46:20 +0000

    more on objects

---
+++ Objects/02_objects.js
@@ -0,0 +1,24 @@
+
+// const student = new Object(); // singleton Object
+const student = {} // non - singleton
+
+student.id = "CCQ946";
+student.name = "Maghan";
+student.isProgrammer = true;
+
+// console.log(student);
+
+const emp = {
+    email: "emp@gmail.com",
+    fullName: {
+        usefullName: {
+            firstName: "Das",
+            lastName: "Maghan"
+        }
+    }
+}
+
+// console.log(emp)
+// console.log(emp.fullName)
+// console.log(emp.fullName.usefullName)
+// console.log(emp.fullName.usefullName.firstName)
