f5cb9e42e63f94fc76f339f8a3a0a1cc534a6cc5
Maghan Das <dasmaghan786@gmail.com>
2025-01-28 21:12:53 +0000

    concats

---
+++ Arrays/02_arrays.js
@@ -0,0 +1,8 @@
+
+const webSeries = ["PeakyB", "MoneyHeist" ,"From"];
+const movies = ["PK", "IronMan", "fastX"];
+
+const Netflix = webSeries.concat(movies);
+// concat combines and returns new arr..
+console.log(Netflix);
+
