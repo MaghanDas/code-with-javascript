89f210b2ea4489a6c65f6c779394d3a45dc16aef
Maghan Das <dasmaghan786@gmail.com>
2025-01-28 20:50:35 +0000

    modified arrys

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -4,6 +4,7 @@
 
 const nums = [0, 1, 2, 3, 4, 5];
 const heros = ["shaktiman", "nagraj"];
+// in js , array can have different types of elments! 
 
 let a =  [];
 
@@ -14,4 +15,16 @@ let a =  [];
 let array = new Array(10,20,30);
 console.log(array);
 
+console.log(typeof array) // type of object
+console.log(typeof nums) // type of object 
 
+nums.push(14); // adding elemnt in the end of array.
+nums.unshift(1); // to the beginning 
+
+console.log(nums);
+
+nums.pop() // removes and return the last elements
+nums.shift() // removes and return the first element
+nums.splice(1,2) // remove 2 elements starting from index 1
+
+console.log(nums)
