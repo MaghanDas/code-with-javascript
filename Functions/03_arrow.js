856f22874fe63c67c56c66d7c8242e412a623698
Maghan Das <dasmaghan786@gmail.com>
2025-01-31 22:29:41 +0000

    this keyword in js

--- Functions/03_arrow.js
+++ Functions/03_arrow.js
@@ -1,3 +1,18 @@
 
 // this keyword -> current context refering.
 
+const user = {
+    name: "maghan",
+    age: 21,
+    showMessage: function() {
+        console.log(`${this.name} welcome to website!!`)
+        // console.log(this)
+    }
+}
+
+// console.log(this) // in node environment it's {}, whereas in browser it is window 
+// browser mein global object hota hai ? window object.
+// user.showMessage()
+
+
+
