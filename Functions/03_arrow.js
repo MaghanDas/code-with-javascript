aadbb7a20bec7cd0c61203780b6404301acc8b67
Maghan Das <dasmaghan786@gmail.com>
2025-01-31 22:37:26 +0000

    use of arrow functions

--- Functions/03_arrow.js
+++ Functions/03_arrow.js
@@ -14,5 +14,40 @@ const user = {
 // browser mein global object hota hai ? window object.
 // user.showMessage()
 
+// function chai(){
+//     console.log(this)
+// }
+// function chai(){
+//     let name = "das"
+//     console.log(this.name) // undefined.
+// }
+
+// chai() // -> alot of data that is already availabe in this..
 
 
+// const chai = function(){
+//     let name = "das"
+//     console.log(this.name) // undefined.
+// }
+
+// const chai = () => {
+//     let name = "das"
+//     console.log(this.name); // undefined.
+// }
+// 
+// chai()
+
+// need to understand this in better way for interview problems..
+
+
+// ** ARROW FUNCTIONS >>
+
+// const sum = (num1,num2) => {
+//     return num1+num2;
+// }
+
+// const sum = (num1,num2) => num1+num2; // impicit return.. same ouput
+
+const sum = (num1,num2) => (num1+num2); 
+
+console.log(sum(3,4));
