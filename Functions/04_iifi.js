084016c0d874da4d7cb4e41327952b24cf8b67bf
Maghan Das <dasmaghan786@gmail.com>
2025-01-31 22:53:26 +0000

    iifi in js

---
+++ Functions/04_iifi.js
@@ -0,0 +1,31 @@
+
+// imediately invoked functions expressions.. (IIFE)
+
+(function name() {
+    console.log("connecting..!");
+    
+})()
+
+// the above is known as iffi, no need to call it.!
+// iifi -> 
+// Why Use an IIFE?
+// Avoid Global Scope Pollution
+// Variables declared inside an IIFE are not accessible outside of it, 
+// preventing conflicts with other scripts.
+
+// arrow function iifee
+(() => {
+    console.log("Arrow Function IIFE!");
+})();
+
+// IIFE with Parameters
+((name) => {
+    console.log("Arrow Function IIFE!");
+})('maghan');
+
+// named iifee
+(function myIIFE() {
+    console.log("Named IIFE!");
+})();
+
+
