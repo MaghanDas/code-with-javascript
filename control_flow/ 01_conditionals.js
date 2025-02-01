65725566aca0749e854319bde3156df282767f96
Maghan Das <dasmaghan786@gmail.com>
2025-02-01 23:29:20 +0000

    conditionals

--- control_flow/ 01_conditionals.js
+++ control_flow/ 01_conditionals.js
@@ -1,3 +1,27 @@
 
 // 
 
+// const isprogram = true
+
+// if(isprogram){
+//     console.log("isprogrammer execute");
+// }
+// else{
+//     console.log("not a programmer");
+// }
+
+// const score = 200;
+
+// // == (check the value)
+// // === (checks the value and datatype)
+// // eg: if(2=="2")
+
+// if(score > 100) {
+//     var power = "can fly";
+//     console.log(`user : ${power}`);
+// }
+
+// console.log(`user : ${power}`);
+
+console.log("hello");
+
