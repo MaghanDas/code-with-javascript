8d867cf87341d8643861a5d3d89c59582534876f
Maghan Das <dasmaghan786@gmail.com>
2025-02-01 23:46:13 +0000

    if else in js

---
+++ control_flow/01_flow.js
@@ -0,0 +1,31 @@
+
+// if(true){
+// console.log("hey");
+// }
+
+// if(2=="2"){
+//     console.log("2 equals 2"); // true
+// }
+// else{
+//     console.log("not ewual");   
+// }
+
+
+// 2=="2" // value comparison
+// 2==="2" // value and dateType conversion.
+
+let day = 2;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    default:
+        console.log("not good value");
+        
+        break;
+}
+
+
