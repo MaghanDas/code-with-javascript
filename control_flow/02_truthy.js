b3319718267ab480aac3c3f03fd17f2f1d16aefa
Maghan Das <dasmaghan786@gmail.com>
2025-02-01 23:50:38 +0000

    truthy & false values in js

---
+++ control_flow/02_truthy.js
@@ -0,0 +1,15 @@
+
+// const email = "dasmaghan23@gmail.com"
+
+// if(email){
+//     console.log("email exist");   
+// }
+// else {
+//     console.log("email does not exist");
+// }
+
+// falsy value..
+// false, 0, -0, BigInt 0n, "", null, undefined, NaN 
+
+// truthy values.
+// "0", 'false', " ", [], {}, function(){}
