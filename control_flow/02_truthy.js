1499ab2f9f0378c6e8c991969bb5932b43dc1920
Maghan Das <dasmaghan786@gmail.com>
2025-02-02 00:00:48 +0000

    ternary operator

--- control_flow/02_truthy.js
+++ control_flow/02_truthy.js
@@ -13,3 +13,28 @@
 
 // truthy values.
 // "0", 'false', " ", [], {}, function(){}
+
+
+// below are true
+// false == 0 
+// false == ''
+// 0 == ''
+
+// Nullish Coalescing Operator (??) : null undefined
+let val;
+// val = 5 ?? 10   // 5 
+// val = null ?? 10     // 10
+// val = undefined ?? 15 // 15
+// val = null ?? undefined  // undefined
+
+// console.log(val);
+
+
+
+// ternary operator
+// condition ? true : false
+
+// let score = 23
+// score >=100 ? console.log("high score") : console.log("low");
+
+
